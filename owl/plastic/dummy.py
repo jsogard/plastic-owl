04b180878c7e1a92afcaa025e6e0b187618a4f6b
Joe Sogard <jmsogard@gmail.com>
2018-12-20 20:19:57 -0600

    make basic library view with functionality

--- owl/plastic/dummy.py
+++ owl/plastic/dummy.py
@@ -3,253 +3,353 @@
 
 def get_library_data():
 	return [{
-		  "id": 1,
-		  "title": "nullam varius nulla facilisi",
-		  "artist": "Maidment",
-		  "length": 152
-		}, {
-		  "id": 2,
-		  "title": "pede",
-		  "artist": "Lackie",
-		  "length": 217
-		}, {
-		  "id": 3,
-		  "title": "eget vulputate ut ultrices",
-		  "artist": "McGregor",
-		  "length": 29
-		}, {
-		  "id": 4,
-		  "title": "aliquam sit",
-		  "artist": "De Fraine",
-		  "length": 193
-		}, {
-		  "id": 5,
-		  "title": "nibh",
-		  "artist": "MacCumeskey",
-		  "length": 36
-		}, {
-		  "id": 6,
-		  "title": "a nibh in quis",
-		  "artist": "Currer",
-		  "length": 236
-		}, {
-		  "id": 7,
-		  "title": "accumsan",
-		  "artist": "O'Scully",
-		  "length": 223
-		}, {
-		  "id": 8,
-		  "title": "ac est",
-		  "artist": "Sanbroke",
-		  "length": 60
-		}, {
-		  "id": 9,
-		  "title": "adipiscing molestie",
-		  "artist": "Matts",
-		  "length": 241
-		}, {
-		  "id": 10,
-		  "title": "cras pellentesque volutpat dui",
-		  "artist": "Fairlaw",
-		  "length": 205
-		}, {
-		  "id": 11,
-		  "title": "ut suscipit",
-		  "artist": "Rowlands",
-		  "length": 47
-		}, {
-		  "id": 12,
-		  "title": "quis justo",
-		  "artist": "Sowrah",
-		  "length": 78
-		}, {
-		  "id": 13,
-		  "title": "nulla sed",
-		  "artist": "Cumesky",
-		  "length": 176
-		}, {
-		  "id": 14,
-		  "title": "dolor morbi vel lectus",
-		  "artist": "Saltmarshe",
-		  "length": 149
-		}, {
-		  "id": 15,
-		  "title": "in hac habitasse platea",
-		  "artist": "Goldsberry",
-		  "length": 139
-		}, {
-		  "id": 16,
-		  "title": "quam pharetra",
-		  "artist": "Fessier",
-		  "length": 274
-		}, {
-		  "id": 17,
-		  "title": "turpis eget elit sodales",
-		  "artist": "Coye",
-		  "length": 300
-		}, {
-		  "id": 18,
-		  "title": "ante",
-		  "artist": "Bello",
-		  "length": 43
-		}, {
-		  "id": 19,
-		  "title": "faucibus orci",
-		  "artist": "Minchindon",
-		  "length": 257
-		}, {
-		  "id": 20,
-		  "title": "viverra diam vitae quam",
-		  "artist": "Neads",
-		  "length": 171
-		}, {
-		  "id": 21,
-		  "title": "primis in faucibus orci",
-		  "artist": "Vaggs",
-		  "length": 74
-		}, {
-		  "id": 22,
-		  "title": "justo sit amet",
-		  "artist": "Szubert",
-		  "length": 62
-		}, {
-		  "id": 23,
-		  "title": "auctor gravida sem praesent",
-		  "artist": "Careswell",
-		  "length": 274
-		}, {
-		  "id": 24,
-		  "title": "purus phasellus",
-		  "artist": "McLay",
-		  "length": 168
-		}, {
-		  "id": 25,
-		  "title": "cum",
-		  "artist": "Sumshon",
-		  "length": 285
-		}, {
-		  "id": 26,
-		  "title": "pellentesque viverra pede ac",
-		  "artist": "Borgne",
-		  "length": 49
-		}, {
-		  "id": 27,
-		  "title": "lacus",
-		  "artist": "Seamons",
-		  "length": 24
-		}, {
-		  "id": 28,
-		  "title": "magna vestibulum aliquet",
-		  "artist": "Plover",
-		  "length": 218
-		}, {
-		  "id": 29,
-		  "title": "convallis nunc proin at",
-		  "artist": "Lampaert",
-		  "length": 97
-		}, {
-		  "id": 30,
-		  "title": "turpis elementum",
-		  "artist": "Kevlin",
-		  "length": 84
-		}, {
-		  "id": 31,
-		  "title": "et ultrices posuere cubilia",
-		  "artist": "Camelia",
-		  "length": 213
-		}, {
-		  "id": 32,
-		  "title": "ligula in lacus curabitur",
-		  "artist": "Jebb",
-		  "length": 68
-		}, {
-		  "id": 33,
-		  "title": "mollis",
-		  "artist": "Iuorio",
-		  "length": 64
-		}, {
-		  "id": 34,
-		  "title": "nibh fusce lacus purus",
-		  "artist": "Feldklein",
-		  "length": 272
-		}, {
-		  "id": 35,
-		  "title": "leo",
-		  "artist": "Ruprechter",
-		  "length": 237
-		}, {
-		  "id": 36,
-		  "title": "posuere cubilia curae",
-		  "artist": "Maggorini",
-		  "length": 128
-		}, {
-		  "id": 37,
-		  "title": "tristique",
-		  "artist": "Greenman",
-		  "length": 157
-		}, {
-		  "id": 38,
-		  "title": "integer tincidunt ante vel",
-		  "artist": "Humphrys",
-		  "length": 165
-		}, {
-		  "id": 39,
-		  "title": "quam",
-		  "artist": "Needs",
-		  "length": 5
-		}, {
-		  "id": 40,
-		  "title": "in hac habitasse",
-		  "artist": "Mulcock",
-		  "length": 95
-		}, {
-		  "id": 41,
-		  "title": "sit",
-		  "artist": "McCarney",
-		  "length": 8
-		}, {
-		  "id": 42,
-		  "title": "elementum pellentesque quisque porta",
-		  "artist": "Dabling",
-		  "length": 276
-		}, {
-		  "id": 43,
-		  "title": "luctus tincidunt nulla mollis",
-		  "artist": "Onions",
-		  "length": 80
-		}, {
-		  "id": 44,
-		  "title": "penatibus et magnis",
-		  "artist": "Waskett",
-		  "length": 105
-		}, {
-		  "id": 45,
-		  "title": "viverra dapibus nulla",
-		  "artist": "Bunch",
-		  "length": 250
-		}, {
-		  "id": 46,
-		  "title": "sit amet lobortis",
-		  "artist": "Yurchenko",
-		  "length": 89
-		}, {
-		  "id": 47,
-		  "title": "lectus vestibulum quam",
-		  "artist": "Yukhtin",
-		  "length": 104
-		}, {
-		  "id": 48,
-		  "title": "dolor vel est donec",
-		  "artist": "Sivewright",
-		  "length": 71
-		}, {
-		  "id": 49,
-		  "title": "duis bibendum morbi",
-		  "artist": "Goulter",
-		  "length": 92
-		}, {
-		  "id": 50,
-		  "title": "primis in faucibus",
-		  "artist": "Maryska",
-		  "length": 295
-		}]
+	  "id": 1,
+	  "title": "molestie",
+	  "artist": "Skoyles",
+	  "length": 161673,
+	  "order": 1,
+	  "added": "12/21/2014"
+	}, {
+	  "id": 2,
+	  "title": "turpis adipiscing lorem vitae",
+	  "artist": "Drinkhall",
+	  "length": 239693,
+	  "order": 2,
+	  "added": "2/6/2017"
+	}, {
+	  "id": 3,
+	  "title": "in sagittis",
+	  "artist": "Lindemann",
+	  "length": 110202,
+	  "order": 3,
+	  "added": "9/29/2016"
+	}, {
+	  "id": 4,
+	  "title": "est phasellus sit amet",
+	  "artist": "Yeld",
+	  "length": 226125,
+	  "order": 4,
+	  "added": "1/3/2016"
+	}, {
+	  "id": 5,
+	  "title": "ac",
+	  "artist": "Faithfull",
+	  "length": 123596,
+	  "order": 5,
+	  "added": "2/27/2017"
+	}, {
+	  "id": 6,
+	  "title": "ipsum primis in faucibus",
+	  "artist": "Mahomet",
+	  "length": 206741,
+	  "order": 6,
+	  "added": "9/5/2016"
+	}, {
+	  "id": 7,
+	  "title": "nisi venenatis tristique",
+	  "artist": "Daintier",
+	  "length": 61994,
+	  "order": 7,
+	  "added": "2/18/2016"
+	}, {
+	  "id": 8,
+	  "title": "lectus pellentesque",
+	  "artist": "Shackesby",
+	  "length": 236774,
+	  "order": 8,
+	  "added": "3/29/2015"
+	}, {
+	  "id": 9,
+	  "title": "duis ac nibh",
+	  "artist": "Willavoys",
+	  "length": 104386,
+	  "order": 9,
+	  "added": "12/15/2014"
+	}, {
+	  "id": 10,
+	  "title": "massa id nisl venenatis lacinia",
+	  "artist": "Matsell",
+	  "length": 78015,
+	  "order": 10,
+	  "added": "4/5/2016"
+	}, {
+	  "id": 11,
+	  "title": "lacus curabitur at ipsum ac",
+	  "artist": "Kelmere",
+	  "length": 165457,
+	  "order": 11,
+	  "added": "3/28/2016"
+	}, {
+	  "id": 12,
+	  "title": "metus vitae",
+	  "artist": "Renon",
+	  "length": 151875,
+	  "order": 12,
+	  "added": "6/11/2018"
+	}, {
+	  "id": 13,
+	  "title": "leo rhoncus sed",
+	  "artist": "Nurny",
+	  "length": 73610,
+	  "order": 13,
+	  "added": "9/16/2017"
+	}, {
+	  "id": 14,
+	  "title": "molestie",
+	  "artist": "Shickle",
+	  "length": 138835,
+	  "order": 14,
+	  "added": "4/14/2016"
+	}, {
+	  "id": 15,
+	  "title": "dui vel sem sed sagittis",
+	  "artist": "Penberthy",
+	  "length": 85396,
+	  "order": 15,
+	  "added": "7/16/2016"
+	}, {
+	  "id": 16,
+	  "title": "donec",
+	  "artist": "Gerrets",
+	  "length": 178336,
+	  "order": 16,
+	  "added": "8/5/2016"
+	}, {
+	  "id": 17,
+	  "title": "in libero",
+	  "artist": "Kestian",
+	  "length": 165449,
+	  "order": 17,
+	  "added": "3/12/2016"
+	}, {
+	  "id": 18,
+	  "title": "volutpat sapien arcu sed augue",
+	  "artist": "Bigland",
+	  "length": 71866,
+	  "order": 18,
+	  "added": "1/29/2017"
+	}, {
+	  "id": 19,
+	  "title": "sit amet",
+	  "artist": "Harridge",
+	  "length": 103373,
+	  "order": 19,
+	  "added": "7/20/2015"
+	}, {
+	  "id": 20,
+	  "title": "amet",
+	  "artist": "Nazair",
+	  "length": 120558,
+	  "order": 20,
+	  "added": "10/28/2017"
+	}, {
+	  "id": 21,
+	  "title": "justo etiam pretium iaculis",
+	  "artist": "Glowacki",
+	  "length": 161747,
+	  "order": 21,
+	  "added": "8/14/2018"
+	}, {
+	  "id": 22,
+	  "title": "orci",
+	  "artist": "Brickhill",
+	  "length": 166881,
+	  "order": 22,
+	  "added": "10/10/2015"
+	}, {
+	  "id": 23,
+	  "title": "at ipsum ac tellus semper",
+	  "artist": "Brimblecomb",
+	  "length": 182586,
+	  "order": 23,
+	  "added": "12/11/2017"
+	}, {
+	  "id": 24,
+	  "title": "convallis",
+	  "artist": "Lahive",
+	  "length": 136235,
+	  "order": 24,
+	  "added": "5/8/2015"
+	}, {
+	  "id": 25,
+	  "title": "ultrices mattis odio donec",
+	  "artist": "Hultberg",
+	  "length": 81826,
+	  "order": 25,
+	  "added": "2/6/2015"
+	}, {
+	  "id": 26,
+	  "title": "nullam orci pede",
+	  "artist": "Lideard",
+	  "length": 208959,
+	  "order": 26,
+	  "added": "4/18/2017"
+	}, {
+	  "id": 27,
+	  "title": "natoque penatibus et magnis",
+	  "artist": "Aveline",
+	  "length": 61459,
+	  "order": 27,
+	  "added": "4/28/2017"
+	}, {
+	  "id": 28,
+	  "title": "enim blandit mi",
+	  "artist": "Scimoni",
+	  "length": 202962,
+	  "order": 28,
+	  "added": "8/12/2015"
+	}, {
+	  "id": 29,
+	  "title": "ante ipsum primis in faucibus",
+	  "artist": "Dobel",
+	  "length": 133676,
+	  "order": 29,
+	  "added": "12/14/2018"
+	}, {
+	  "id": 30,
+	  "title": "ut",
+	  "artist": "Renol",
+	  "length": 157562,
+	  "order": 30,
+	  "added": "1/3/2016"
+	}, {
+	  "id": 31,
+	  "title": "consequat",
+	  "artist": "Zapata",
+	  "length": 190715,
+	  "order": 31,
+	  "added": "9/2/2017"
+	}, {
+	  "id": 32,
+	  "title": "dolor quis",
+	  "artist": "Risson",
+	  "length": 66293,
+	  "order": 32,
+	  "added": "10/9/2015"
+	}, {
+	  "id": 33,
+	  "title": "ut",
+	  "artist": "Peart",
+	  "length": 100571,
+	  "order": 33,
+	  "added": "4/30/2015"
+	}, {
+	  "id": 34,
+	  "title": "tristique in tempus sit amet",
+	  "artist": "Dockwra",
+	  "length": 176793,
+	  "order": 34,
+	  "added": "8/12/2018"
+	}, {
+	  "id": 35,
+	  "title": "eget tincidunt eget tempus vel",
+	  "artist": "Stitle",
+	  "length": 177020,
+	  "order": 35,
+	  "added": "8/31/2016"
+	}, {
+	  "id": 36,
+	  "title": "sed accumsan",
+	  "artist": "Mayne",
+	  "length": 196556,
+	  "order": 36,
+	  "added": "5/30/2018"
+	}, {
+	  "id": 37,
+	  "title": "etiam pretium iaculis",
+	  "artist": "McPaike",
+	  "length": 71467,
+	  "order": 37,
+	  "added": "8/10/2017"
+	}, {
+	  "id": 38,
+	  "title": "amet diam",
+	  "artist": "Baker",
+	  "length": 158062,
+	  "order": 38,
+	  "added": "5/13/2017"
+	}, {
+	  "id": 39,
+	  "title": "in hac",
+	  "artist": "Tooker",
+	  "length": 144435,
+	  "order": 39,
+	  "added": "10/23/2017"
+	}, {
+	  "id": 40,
+	  "title": "quam turpis adipiscing lorem",
+	  "artist": "Fellow",
+	  "length": 106808,
+	  "order": 40,
+	  "added": "1/15/2017"
+	}, {
+	  "id": 41,
+	  "title": "consectetuer eget",
+	  "artist": "Spriggs",
+	  "length": 221736,
+	  "order": 41,
+	  "added": "12/19/2017"
+	}, {
+	  "id": 42,
+	  "title": "dolor sit",
+	  "artist": "Sharrem",
+	  "length": 193379,
+	  "order": 42,
+	  "added": "1/20/2015"
+	}, {
+	  "id": 43,
+	  "title": "faucibus orci luctus",
+	  "artist": "Ashburner",
+	  "length": 239091,
+	  "order": 43,
+	  "added": "10/29/2017"
+	}, {
+	  "id": 44,
+	  "title": "primis",
+	  "artist": "Cryer",
+	  "length": 197950,
+	  "order": 44,
+	  "added": "10/24/2016"
+	}, {
+	  "id": 45,
+	  "title": "libero",
+	  "artist": "Cromwell",
+	  "length": 99092,
+	  "order": 45,
+	  "added": "2/27/2017"
+	}, {
+	  "id": 46,
+	  "title": "aliquam",
+	  "artist": "Melato",
+	  "length": 214067,
+	  "order": 46,
+	  "added": "3/2/2018"
+	}, {
+	  "id": 47,
+	  "title": "sit amet turpis elementum ligula",
+	  "artist": "Dommerque",
+	  "length": 198412,
+	  "order": 47,
+	  "added": "3/10/2016"
+	}, {
+	  "id": 48,
+	  "title": "tincidunt eget tempus vel pede",
+	  "artist": "Challin",
+	  "length": 229183,
+	  "order": 48,
+	  "added": "5/10/2018"
+	}, {
+	  "id": 49,
+	  "title": "tellus nisi",
+	  "artist": "Huriche",
+	  "length": 140130,
+	  "order": 49,
+	  "added": "2/16/2018"
+	}, {
+	  "id": 50,
+	  "title": "id",
+	  "artist": "Meineken",
+	  "length": 61142,
+	  "order": 50,
+	  "added": "12/15/2016"
+	}]
--- owl/static/plastic/library.js
+++ owl/static/plastic/library.js
@@ -1,8 +1,39 @@
 $(document).ready(function(){
 
-	let library = [];
+	let LIBRARY = [];
+	const STYLE = { LIST: 'LIST', GRID: 'GRID' };
+	let POPPER = null;
 
-	const get_song_dom = function(song) {
+	const Song = function(json) {
+		const _ = function (content) {
+			return '<span class=\'td-content\'>' + content + '</span>';
+		};
+
+		this.order = _(json.order) + '<span class="glyphicon glyphicon-play-circle" aria-hidden="true"></span>';
+		this.title = _(json.title) + '<span class="glyphicon glyphicon-option-vertical float-right" aria-hidden="true"></span>';
+		this.artist = _(json.artist);
+		this.added = _(json.added);
+		this.length = _(json.length);
+	}
+
+	// const get_song_dom_list = function(song) {
+
+	// 	return $('<tr></tr>')
+	// 		.addClass("song")
+	// 		.append($('<td></td>'))
+	// 		.append($('<td></td>')
+	// 			.text(song.title))
+	// 		.append($('<td></td>')
+	// 			.text(song.artist))
+	// 		.append($('<td></td>')
+	// 			.text(0))
+	// 		.append($('<td></td>'))
+	// 		.append($('<td></td>')
+	// 			.text(0))
+	// 		;
+	// }
+
+	const get_song_dom_grid = function(song) {
 
 		return $('<div></div>')
 			.addClass("col-3")
@@ -19,20 +50,130 @@ $(document).ready(function(){
 	const get_all_songs = function(callback) {
 
 		$.getJSON('api/songs', function(data) {
-			library = data;
-			populate_library_doms();
+			$.each(data, function(index, song) {
+				LIBRARY.push(new Song(song));
+			});
+			// LIBRARY = data;
+			populate_library_doms(STYLE.LIST);
 			if(callback) callback();
 		});
 	};
 
-	const populate_library_doms = function() {
-		const $content = $('div#content');
-
-		$.each(library, function(index, song) {
-			$content.append(get_song_dom(song));
+	const populate_library_doms = function(style, sort, order) {
+		
+		$('table#library-table').DataTable({
+			data: LIBRARY,
+			columns: [
+				{ data: 'order' },
+				{ data: 'title' },
+				{ data: 'artist' },
+				{ data: 'added' },
+				{ data: 'length' },
+			]
 		});
 	}
 
-	get_all_songs();
+	$('.dropdown-menu').hide();
+	let SELECTED = null;
+
+	get_all_songs(function callback() {
+		const $dropdown = $('.dropdown-menu');
+		let popper = null;
+
+		const timeoutHideMenu = function() {
+			const timeout = setTimeout(function() {
+				$dropdown.hide();
+				console.log('dropdown timeout fulfilled');
+			}, 50);
+			$dropdown.data('timeout', timeout);
+			console.log('dropdown timeout started with id ' + timeout);
+		};
+
+		const cancelTimeout = function() {
+			const timeout = $dropdown.data('timeout');
+			clearTimeout(timeout);
+			console.log('dropdwon timeout cancelled with id ' + timeout);
+		};
+
+		$dropdown.hover(cancelTimeout, function() { cancelTimeout(); timeoutHideMenu(); });
+
+		$('tbody tr').click(function() {
+			$('tbody tr.selected').removeClass('selected');
+			$(this).addClass('selected');
+			console.log('dropdonw shown, selected class put on row');
+		}).mouseleave(function() {
+			if($(this).hasClass('selected')) {
+				cancelTimeout();
+				console.log('mouse leave for selected row');
+				timeoutHideMenu();
+			}
+		});
+
+		$('tbody tr td span.glyphicon-option-vertical').click(function() {
+			popper = new Popper($(this), $dropdown, {});
+			$dropdown.show();
+		});
+
+		$('#dropdown > *').click(function() {
+			console.log($(this));
+			console.log('clicked the dropdown');
+			if(popper) popper.destroy();
+		})
+
+
+		// when i click the ellipse, show the menu and set row as selected
+
+		// when i move off of the selected row it sets a timer to fade out the menu $id
+
+		// when i move into the menu it cancels the timer with $id
+
+		// when i move off of the menu it starts the same timer $id
+
+
+		// const mouseIn = function () {
+		// 	if(POPPER) {
+
+		// 		clearTimeout(dropdown.data('timeoutId'));
+		// 		console.log('clear timeout: ' + dropdown.data('timeoutId'));
+		// 	}
+		// }, mouseOut = function () {
+		// 	if(POPPER) {
+
+		// 		console.log('mosueout');
+		// 		let timeoutId = setTimeout(function() {
+		// 			dropdown.hide();
+		// 		}, 1000)
+		// 		dropdown.data('timeoutId', timeoutId);
+		// 		console.log('attach timeout: ' + timeoutId);
+		// 	}
+		// };
+
+
+		// $('tbody tr').click(function() {
+
+		// 	if(SELECTED) SELECTED.removeClass('selected');
+		// 	SELECTED = $(this);
+		// 	$(this).addClass('selected');
+		// 	// $(this).mouseIn(mouseIn);
+		// 	$(this).mouseleave(mouseOut);
+		// })
+		
+		// $('tbody tr td:nth-child(2) span:not(.td-content)').click(function() {
+
+		// 	POPPER = new Popper(
+		// 		this,
+		// 		dropdown,
+		// 		{ placement: 'auto-end' });
+		// 	dropdown.show();
+		// });
+
+
+		// $('.dropdown-menu').mouseleave(mouseOut);
+		// $('.dropdown-menu').mouseenter(mouseIn);
+
+
+
+	});
+
 
 });
